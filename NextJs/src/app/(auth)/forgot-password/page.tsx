673da33ca6042b6482973f2577fe147d6a0b6ffd
DevEren <dev.erenildo@gmail.com>
2024-03-14 19:08:54 -0400

    route groups

---
+++ NextJs/src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+const ForgotPassword = () => {
+    return(
+        <h1>forgot password</h1>
+    );
+}
+
+export default ForgotPassword;
---
+++ NextJs/src/app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+const Register = () => {
+    return(
+        <h1>register page</h1>
+    );
+}
+
+export default Register;
