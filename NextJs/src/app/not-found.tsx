5a06d26129a897811b6a21fd9919fbb066f05f58
DevEren <dev.erenildo@gmail.com>
2024-03-14 18:42:42 -0400

    not found page

---
+++ NextJs/src/app/not-found.tsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+    return(
+        <>
+            <h1>
+                pagina não encontrada
+            </h1>
+            <p>
+                tente outra rota 
+            </p>
+        </>
+    );
+}
+
+export default NotFound;
