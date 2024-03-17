efb4e0143662213562708e5c39ac0dc72537f045
DevEren <dev.erenildo@gmail.com>
2024-03-17 16:17:28 -0400

    nested layouts

---
+++ NextJs/src/app/products/[productId]/layout.tsx
@@ -0,0 +1,12 @@
+const ProductDetailsLayout = ({ children } : {
+    children: React.ReactNode
+}) => {
+    return(
+        <>
+            {children}
+            <h1>test layout product</h1>
+        </>
+    );
+}
+
+export default ProductDetailsLayout;
