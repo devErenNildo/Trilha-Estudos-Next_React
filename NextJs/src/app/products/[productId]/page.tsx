a141b6793d49294935d61b38cb494e4fc4287247
DevEren <dev.erenildo@gmail.com>
2024-03-17 16:59:17 -0400

    routing metadata

--- NextJs/src/app/products/[productId]/page.tsx
+++ NextJs/src/app/products/[productId]/page.tsx
@@ -1,6 +1,19 @@
-const ProductDetails = ({ params }: {
-    params: { productId: string}
-}) => {
+import { Metadata } from "next";
+
+type Props = {
+    params: {
+        productId: string
+    }
+}
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+    return{
+        title: `Product ${params.productId}`
+    }
+}
+
+
+const ProductDetails = ({ params }: Props) => {
     return(
         <h1>detalhes produto lista {params.productId}</h1>
     );
